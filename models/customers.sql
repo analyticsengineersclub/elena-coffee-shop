92a1bba49e5d7f4614fe18b14951baa66928408f
Ubuntu <ubuntu@ip-172-31-28-161.us-east-2.compute.internal>
2021-08-29 21:54:22 +0000

    Updating model configs

---
+++ models/customers.sql
@@ -0,0 +1,17 @@
+with customer_orders as (
+    select  customer_id,
+            min(created_at) as first_order_at,
+            count(distinct id) as number_of_orders 
+    from `analytics-engineers-club.coffee_shop.orders`
+    group by 1 
+    order by 1)
+
+select  customers.id as customer_id,
+        customers.name,
+        customers.email,
+        customer_orders.first_order_at,
+        customer_orders.number_of_orders 
+from `analytics-engineers-club.coffee_shop.customers` customers 
+inner join customer_orders
+on customers.id = customer_orders.customer_id
+order by 4 asc;
