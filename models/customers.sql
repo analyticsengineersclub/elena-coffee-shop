7c1deafba9a2952412b7b62a0ac5745f8f61af5d
Ubuntu <ubuntu@ip-172-31-28-161.us-east-2.compute.internal>
2021-09-01 03:39:29 +0000

    exercise 2 completed

--- models/customers.sql
+++ models/customers.sql
@@ -6,7 +6,7 @@ with customer_orders as (
     select  customer_id,
             min(created_at) as first_order_at,
             count(distinct id) as number_of_orders 
-    from `analytics-engineers-club.coffee_shop.orders`
+    from {{ source ('coffee_shop','orders') }}
     group by 1 
     order by 1)
 
@@ -15,7 +15,7 @@ select  customers.id as customer_id,
         customers.email,
         customer_orders.first_order_at,
         customer_orders.number_of_orders 
-from `analytics-engineers-club.coffee_shop.customers` customers 
+from {{ source ('coffee_shop','customers') }} customers 
 inner join customer_orders
 on customers.id = customer_orders.customer_id
 order by 4 asc
