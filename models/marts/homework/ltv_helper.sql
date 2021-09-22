b3573927e0b20ee12afc748eb7e5122e44b7c0da
ElenaRusAthletx <dyachkova.elena@gmail.com>
2021-09-22 02:38:12 +0000

    adding rolling total to ltv helper and cleaning up some documentation

--- models/marts/homework/ltv_helper.sql
+++ models/marts/homework/ltv_helper.sql
@@ -14,8 +14,9 @@ weekly_skeleton as (
         ltv_helper.week_count
     from first_orders  
     cross join {{ref('ltv_helper_week_range')}} ltv_helper
-    order by 1 asc, 3 asc)
+    order by 1 asc, 3 asc),
 
+weekly_revenue as (
 select 
     weekly_skeleton.customer_id,
     weekly_skeleton.first_order_date,
@@ -28,4 +29,15 @@ left join {{ref('order_items')}} order_items
     order_items.order_date >= date_add(weekly_skeleton.first_order_date, interval 7*(weekly_skeleton.week_count-1) day)  and 
     order_items.order_date < date_add(weekly_skeleton.first_order_date, interval 7*weekly_skeleton.week_count day)
 group by 1,2,3,4
-order by 1 asc, 4 asc 
+order by 1 asc, 4 asc)
+
+select 
+    customer_id,
+    first_order_date,
+    first_order_week,
+    week_count,
+    total_revenue as weekly_revenue,
+    sum(total_revenue) over (partition by customer_id order by week_count asc rows unbounded preceding) as lifetime_revenue 
+from weekly_revenue
+order by 1 asc, 4 asc
+
